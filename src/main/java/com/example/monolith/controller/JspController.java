c947185eb0aca1d8858e4e91f1457cb330eb4276
MotivusHugoB <hugo.buenrostroa@motivus.com>
2025-04-02 16:32:06 -0600

    init: adding initial files

---
+++ src/main/java/com/example/monolith/controller/JspController.java
@@ -0,0 +1,13 @@
+package com.example.monolith.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JspController {
+
+    @GetMapping("/jsp/hello")
+    public String helloJsp() {
+        return "hello";
+    }
+}
